3d035e779127f7b8a12753fe719dee1e86866a73
Lizzie Siegle <esiegle@brynmawr.edu>
2025-02-02 19:51:26 +0100

    basic vday

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import html from "../static/index.html";
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		if (request.url.includes("/api")) {
+			const messages = [
+				{ role: "system", content: "You are a friendly assistant who generates valentines messages" },
+				{
+				  role: "user",
+				  content: "Return a simple valentine's day poem for the user",
+				},
+			  ];
+			  const response = await env.AI.run("@cf/meta/llama-3.3-70b-instruct-fp8-fast", { messages });
+			  console.log(response['response']);
+			  return Response.json(response['response']);
+		}
+		return new Response(html, {
+			headers: {
+				"Content-Type": "text/html",
+			},
+		});
 	},
 };
